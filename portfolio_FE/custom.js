aadd2c4879eb695b1b7b7487652a99789f8beef6
heeyeon_kim <54020487+heeyeon-developer@users.noreply.github.com>
2020-09-09 20:40:38 +0900

    2020.09.09_3

--- portfolio_FE/custom.js
+++ portfolio_FE/custom.js
@@ -3,4 +3,14 @@ $(function(){
         var position = $("#introduce_location".offset());
         $("body").stop().animate({scrollTop:position.top},500);
     })
+    //Slick.js :experience
+    $('.experience_info').slick({
+        dots: true,
+        infinite: true,
+        speed: 500,
+        fade: true,
+        autoplay: true,
+        autoplayspeed: 2000,
+        cssEase: 'linear'
+    })
 })
